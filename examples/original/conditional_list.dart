bd94eb0cbcf9c4f1ff5fe85d022d0125b619cc7a
Bob Nystrom <rnystrom@google.com>
2018-06-28 17:58:35 -0700

    Tweak original examples.

--- examples/original/conditional_list.dart
+++ examples/original/conditional_list.dart
@@ -1,10 +1,7 @@
 Widget build(BuildContext context) {
   IconButton button;
   if (isWindows) {
-    button = IconButton(
-      icon: Icon(Icons.menu),
-      tooltip: 'Navigation menu',
-    );
+    button = IconButton(icon: Icon(Icons.menu), tooltip: 'Navigation menu');
   } else {
     button = IconButton(
       icon: Icon(Icons.menu),
@@ -13,26 +10,18 @@ Widget build(BuildContext context) {
     );
   }
 
-  List<Widget> buttons = [
-    button,
-    Expanded(
-      child: title,
-    ),
-  ];
+  List<Widget> buttons = [button, Expanded(child: title)];
 
   if (!isAndroid) {
-    buttons.add(IconButton(
-      icon: Icon(Icons.search),
-      tooltip: 'Search',
-    ));
+    buttons.add(IconButton(icon: Icon(Icons.search), tooltip: 'Search'));
   }
 
   return Container(
     height: 56.0,
     padding: const EdgeInsets.symmetric(horizontal: 8.0),
     decoration: BoxDecoration(color: Colors.blue[500]),
-    child: Row(
-      children: buttons,
-    ),
+    child: Row(children: buttons),
   );
 }
+
+// - removed trailing commas in places where all args fit on one line
